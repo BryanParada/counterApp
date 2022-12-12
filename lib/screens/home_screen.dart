739f54f14c7269ce708a16f2f3788a02fade15a7
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-12 12:41:35 -0300

    F47 - BuildContext - Scaffold

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,20 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text('Hello from Home_screen'),
-      );
+    return const Scaffold(
+      backgroundColor: Colors.green,
+      body:
+          Center(
+          child: Text('Hello from Home_screen'),
+        )
+
+    );
   }
 
 }
