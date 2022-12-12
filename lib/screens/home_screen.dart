78dba983c08a8a45dd907315facbdfae04557b08
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-12 15:31:54 -0300

    F50 - text style size

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,10 +3,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+ 
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    const fontSize30 = TextStyle(fontSize: 30);
+
     return Scaffold(
       //backgroundColor: Colors.green,
       appBar: AppBar(
@@ -17,9 +21,9 @@ class HomeScreen extends StatelessWidget {
           Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,  
-              children: const <Widget>[
-              Text('Count Clicks'),
-              Text('asdf'), 
+              children: const <Widget>[ //const <Widget>
+              Text('Count Clicks', style: fontSize30),
+              Text('10', style: fontSize30), 
             ],
             ),
           )
